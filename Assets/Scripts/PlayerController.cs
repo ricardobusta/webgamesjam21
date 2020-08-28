3a6e48812b76e50d40f0a33388f42c2897920540
Ricardo Bustamante <ricardo@busta.dev>
2020-08-28 21:31:24 +0200

    Add base project with fps controller.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float lookSpeed;
+
+    private Rigidbody _rigidbody;
+    private Vector3 _lastMousePos;
+
+    private Vector3 _look;
+
+    [SerializeField] private Transform _camera;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        _rigidbody = GetComponent<Rigidbody>();
+        _lastMousePos = Input.mousePosition;
+    }
+
+    private void Move()
+    {
+        var t = transform;
+        var lookAngle = _look.y * Mathf.Deg2Rad;
+        var angleSin = Mathf.Sin(lookAngle);
+        var angleCos = Mathf.Cos(lookAngle);
+
+        var vertical = Input.GetAxis("Vertical") * movementSpeed;
+        var horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        
+        var forwardSpeed = angleSin * vertical + angleCos * horizontal;
+        var strafeSpeed = angleCos * vertical - angleSin * horizontal;
+
+        var movement = forwardSpeed + strafeSpeed;
+        var verticalSpeed = _rigidbody.velocity.y;
+        _rigidbody.velocity = new Vector3(forwardSpeed,verticalSpeed,strafeSpeed);
+    }
+
+    private void Look()
+    {
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+        
+        // Mouse horizontal control camera rotation in vertical axis and vice versa
+        _look = new Vector3(
+            Mathf.Clamp(_look.x - mouseY * lookSpeed, -90, 90),
+            _look.y + mouseX * lookSpeed
+        );
+
+        _camera.rotation = Quaternion.Euler(_look);
+    }
+
+    private void Update()
+    {
+        Look();
+        Move();
+    }
+}
