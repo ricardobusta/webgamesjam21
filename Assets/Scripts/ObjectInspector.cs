0f51343d1d3a249def7cbcb21e273a1e8c1777f0
Ricardo Bustamante <ricardo@busta.dev>
2020-08-29 15:36:11 +0200

    Tune physics, attempt to make highlight work

--- Assets/Scripts/ObjectInspector.cs
+++ Assets/Scripts/ObjectInspector.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ObjectInspector : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro _inspectLabel;
-    
+    [SerializeField] private TextMeshPro inspectLabel;
+    [SerializeField] private LayerMask interactiveLayer;
+
+    [SerializeField] private float interactionDistance;
+
     private Interactable _inpsectedObject;
     private bool _hasInspectedObject;
 
+    private RaycastHit[] _hits = new RaycastHit[1];
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var t = transform;
+        var ray = new Ray(t.position, t.forward);
+        Debug.DrawRay(t.position, t.forward * interactionDistance);
+        if (Physics.RaycastNonAlloc(ray, _hits, interactionDistance, interactiveLayer) > 0)
+        {
+            if (_hits[0].collider.TryGetComponent(out _inpsectedObject))
+                {
+                    _hasInspectedObject = true;
+                    _inpsectedObject.gameObject.layer = LayerMask.NameToLayer("Outline");
+                    Debug.Log("Setting Layer");
+                }
+        }
+        else
+        {
+            if (_hasInspectedObject)
+            {
+                _inpsectedObject.gameObject.layer = LayerMask.NameToLayer("Default");
+                _hasInspectedObject = false;
+                Debug.Log("Removing Layer");
+            }
+        }
     }
 }
